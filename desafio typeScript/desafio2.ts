54c0a36b58fafe1f5d804817f3b6b7f019b93a37
Thiago Bezerra <101569921+Thiagobezerrabarbosa@users.noreply.github.com>
2022-06-10 20:39:23 -0300

    Add files via upload

---
+++ desafio typeScript/desafio2.ts
@@ -0,0 +1,29 @@
+// Como podemos melhorar o esse código usando TS? 
+
+interface dados{
+    nome: string,
+    idade: number,
+    profissao: string
+}
+
+let pessoa1: dados = {
+nome : 'maria',
+idade : 29,
+profissao : 'atriz'
+}
+let pessoa2: dados = {
+nome : 'roberto',
+idade : 19,
+profissao : 'Padeiro'
+}
+let pessoa3 : dados = {
+    nome: 'laura',
+    idade: 32,
+    profissao: 'Atriz'
+}
+
+let pessoa4: dados = { 
+    nome : 'carlos',
+    idade : 19,
+    profissao : 'padeiro'
+}
